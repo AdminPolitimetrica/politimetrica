ec5d26b2b354fd40f4b019fbc4bdf4c3ba89c008
AdminPolitimetrica <politimetrica@gmail.com>
2025-06-07 19:19:16 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
-import withPWA from 'next-pwa'
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
   },
 }
 
-export default withPWA(nextConfig)
+export default nextConfig
