230186b166c03aa9b04e080cc711facaff90a61e
AdminPolitimetrica <politimetrica@gmail.com>
2025-06-17 21:11:04 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -14,7 +14,8 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
-    persistSession: false
+    persistSession: true,
+    detectSessionInUrl: true,
   }
 })
 
